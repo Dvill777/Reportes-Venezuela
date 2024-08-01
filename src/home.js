c8913efdfc2be7d6e378549aa8b982ed12f0d519
Dvill777 <juandsb1703@gmail.com>
2024-08-01 16:32:52 -0500

    tailwind y componente home

---
+++ src/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className='home'>
+      <div className='sec-reportes'>
+        <h1>Reporta una irregularidad</h1>
+        <h2>Desaparición</h2>
+        <h2>Secuestro</h2>
+      </div>
+    </div>
+  )
+}
+
+export default home
